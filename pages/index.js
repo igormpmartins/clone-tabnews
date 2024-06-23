bb27f43f9bbdc1e4e9a1c2e80d8fa8f5dd8b211a
Igor Martins <73676465+igormpmartins@users.noreply.github.com>
2024-06-23 18:57:13 +0000

    new message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Miuuu ♥🐈</h1>
+    return (
+        <>
+            <h1>Miuuu ♥🐈</h1>
+            <h2>Futura PO, AWS certified</h2>
+        </>
+    )
 }
 
 export default Home;
